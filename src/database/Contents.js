22254913ab09a14ea3ac9cdb4acd3528297ad003
Harry Surya <harrysurya93@gmail.com>
2023-08-28 00:11:30 -0700

    fix typo

--- src/database/Contents.js
+++ src/database/Contents.js
@@ -7,7 +7,7 @@ contentAbout.addParagraph(uuidv4(),<>Proven <mark>hands-on experience in gatheri
 
 const contentIntroduction = new ContentIntroduction('Hello World, my name is','Harry Surya','Full Stack Developer','Experienced for 5 years in developing web application for various projects across industries.','https://goo.gl/maps/RqFsQb1WF2reuUhk9','Vancouver, BC, Canada 🇨🇦','About Me','#sectionAbout')
 
-const contentContact = new ContentContact('"Let\'s Connect and Build Together!"', 'Sincerely','Harry Surya','mailto:harrysurya93@gmail.com','Send Email','bi bi-envelope-at-fill');
+const contentContact = new ContentContact('"Let\'s Connect and Build Together!"', 'Sincerely,','Harry Surya','mailto:harrysurya93@gmail.com','Send Email','bi bi-envelope-at-fill');
 contentContact.addParagraph(uuidv4(),'Are you seeking for versatile and skilled full-stack developer to join your team? Look no further! I\'m actively seeking exciting opportunities to contribute my expertise and passion for crafting exceptional digital experiences.');
 contentContact.addParagraph(uuidv4(),'If you\'re interested in collaborating on innovative projects, discussing potential job opportunities, or simply want to connect and share ideas, I would love to hear from you. Feel free to reach out with me on [LinkedIn/GitHub/Email]. Let\'s create something amazing together!');
 contentContact.addParagraph(uuidv4(),'Looking forward to the possibility of working with you.');
