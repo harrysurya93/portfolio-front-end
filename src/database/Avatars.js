7716f76242041af3cb3ee32b4fe40cd265249cbe
Harry Surya <harrysurya93@gmail.com>
2024-03-11 16:47:13 -0700

    update Avatars

--- src/database/Avatars.js
+++ src/database/Avatars.js
@@ -1,12 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
 import Avatar1 from '../assets/images/avatar1.png';
 import Avatar2 from '../assets/images/avatar2.png';
-import Avatar3 from '../assets/images/avatar3.png';
+import Avatar3 from '../assets/images/Harry_Casual.png';
 import Avatar from '../models/Avatar';
 
 const tempAvatar1 = new Avatar(uuidv4(), Avatar1, 'avatar1');
 const tempAvatar2 = new Avatar(uuidv4(), Avatar2, 'avatar2');
-const tempAvatar3 = new Avatar(uuidv4(), Avatar3, 'Harry_Casual');
+const tempAvatar3 = new Avatar(uuidv4(), Avatar3, 'avatar3');
 
 const Avatars = [];
 Avatars.push(tempAvatar1, tempAvatar2, tempAvatar3);
