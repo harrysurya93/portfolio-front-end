259871a7ac2a3e96b8fd9c2e1fd1a33bda1a2380
Harry Surya <harrysurya93@gmail.com>
2024-06-03 00:24:28 -0700

    update work experience

--- src/database/Experiences.js
+++ src/database/Experiences.js
@@ -2,16 +2,16 @@ import { v4 as uuidv4 } from 'uuid';
 import Experience from '../models/Experience';
 
 const Experience1 = new Experience(uuidv4(), 'Polychemie Asia Pacific Permai','https://www.polychemie.co.id','Head of IT Development','Aug 2019','May 2023');
-Experience1.addResponsibility(uuidv4(),'Directed a trio of developers, streamlining web server/app development and aligning team efforts.');
-Experience1.addResponsibility(uuidv4(),'Crafted ERP-integrated software to uphold data accuracy data accuracy and integrity.');
-Experience1.addResponsibility(uuidv4(),'Managing the security of implemented web applications using the latest security solutions.');
-Experience1.addResponsibility(uuidv4(),'Melded IT insights with business operations for strategic enhancements.');
+Experience1.addResponsibility(uuidv4(),'Directed three developers, streamlined web server or application development, coordinated project priorities, and provided mentorship to drive team objectives.');
+Experience1.addResponsibility(uuidv4(),'Combined IT analysis with business processes to create comprehensive strategies to improve operational efficiency and support business decisions.');
+Experience1.addResponsibility(uuidv4(),'Developed integrated API for the core ERP database, ensuring data accuracy and integrity.');
+Experience1.addResponsibility(uuidv4(),'Managed the security of implemented web applications using the latest security solutions.');
 
 const Experience2 = new Experience(uuidv4(),'Soltius Indonesia','https://soltius.co.id','Technical Consultant','Aug 2018','Jul 2019');
-Experience2.addResponsibility(uuidv4(),'Delivered technical support, resolving bugs and issues as per client needs.');
+Experience2.addResponsibility(uuidv4(),'Delivered technical support, resolving bugs and issues according to client needs.');
 Experience2.addResponsibility(uuidv4(),'Worked with team members to implement web application solutions that integrated with the ERP.');
 Experience2.addResponsibility(uuidv4(),'Designed database validations and approvals, ensuring alignment with client processes.');
-Experience2.addResponsibility(uuidv4(),'Presented the final product to key users to ensure the UAT and GO-LIVE phases can be achieved according to the project timeline.');
+Experience2.addResponsibility(uuidv4(),'Presented the final product to key users to ensure the User Acceptance Test (UAT) and Go-Live phaseswere achieved based on the project timeline.');
 
 const Experience3 = new Experience(uuidv4(), 'FreshPoint', 'https://www.freshpoint.com/', 'Production Coordinator', 'Nov 2023','Present');
 Experience3.addResponsibility(uuidv4(),'Coordinate production runs with the production supervisor to ensure the process are cost-effective and timely.');
